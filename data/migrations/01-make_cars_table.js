f98a2c7bb7bb273fc7194803d104c1aa3e71823e
samuel singletary <codysingletary92@gmail.com>
2021-06-08 21:18:48 -0400

    finished migration table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,30 @@
-// DO YOUR MAGIC
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments('id')
+
+        tbl.string('vin', 17)//ADD THE PROPER DIGIT FOR THE VIN NUMBER
+            .notNullable()
+            .unique()
+
+        tbl.string('make', 128)
+            .notNullable()
+
+        tbl.string('model', 128)
+            .notNullable()
+    
+        tbl.integer('mileage', 128)
+            .notNullable()
+        
+        tbl.string('title', 128)
+        
+
+        tbl.string('transmission', 128)
+            
+
+    })
+};
+
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars')
+};
